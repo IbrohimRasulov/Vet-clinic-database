609c25307eaa306a3b02f52511aa4d89ce13fbb0
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-03-25 23:46:06 +0500

    Insert data

--- data.sql
+++ data.sql
@@ -1,5 +1,13 @@
-/* Populate database with sample data. */
+/* Populate database WITH sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO ANIMALS(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES(1, 'Agumon', '3-Feb-2020', 0, true, 10.23);
+
+INSERT INTO ANIMALS(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES(2, 'Gabumon', '15-Nov-2018', 2, true, 8);
+
+INSERT INTO ANIMALS(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES(3, 'Pikachu', '7-Jan-2021', 1, false, 15.04);
+
+INSERT INTO ANIMALS(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES(4, 'Devimon', '12-May-2017', 5, true, 11);
