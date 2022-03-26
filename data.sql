3fc1aa2aecda3001e89c580f7c971a44791e4205
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-03-27 01:20:45 +0500

    Complete queries for milestone 3

--- data.sql
+++ data.sql
@@ -13,3 +13,45 @@ VALUES
   (9, 'Boarmon', '7-JUN-2005', 7, true, 20.4),
   (10, 'Blossom', '13-OCT-1998', 3, true, 17),
   (11, 'Ditto', '14-MAY-2022', 4, true, 22);
+
+INSERT INTO owners(full_name, age)
+VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+INSERT INTO species(name)
+VALUES
+  ('Pokemon'),
+  ('Digimon');
+
+UPDATE ANIMALS
+SET species_id = 1;
+UPDATE ANIMALS
+SET species_id = 2
+WHERE name LIKE '%mon%';
+
+
+UPDATE ANIMALS
+SET owner_id = 1
+WHERE name = 'Agumon';
+SAVEPOINT OWNER_1;
+UPDATE ANIMALS
+SET owner_id = 2
+WHERE name IN ('Gabumon', 'Pikachu');
+SAVEPOINT OWNER_2;
+UPDATE ANIMALS
+SET owner_id = 3
+WHERE name IN ('Devimon', 'Plantmon');
+SAVEPOINT OWNER_3;
+UPDATE ANIMALS
+SET owner_id = 4
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+SAVEPOINT OWNER_4;
+UPDATE ANIMALS
+SET owner_id = 5
+WHERE name IN ('Angemon', 'Boarmon');
+SAVEPOINT OWNER_5;
