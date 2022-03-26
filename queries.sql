63f35d94df4fbceb24ceb62641a55842ea6be31f
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-03-26 21:23:14 +0500

    Complete all queries

--- queries.sql
+++ queries.sql
@@ -41,4 +41,32 @@ DELETE FROM animals;
 ROLLBACK;
 SELECT * from animals;
 
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT deletion;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
 
+/* How many animals are there? */
+SELECT COUNT(*) FROM animals;
+
+/* How many animals have never tried to escape? */
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+/* What is the average weight of animals? */
+SELECT AVG(weight_kg) FROM animals;
+
+/* Who escapes the most, neutered or not neutered animals? */
+SELECT neutered, SUM(escape_attempts) FROM animals
+GROUP BY neutered;
+
+/* What is the minimum and maximum weight of each type of animal? */
+SELECT species, MIN(weight_kg) FROM animals
+GROUP BY species;
+
+/* What is the average number of escape attempts per animal type of those born between 1990 and 2000? */
+SELECT species, AVG(escape_attempts) FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
