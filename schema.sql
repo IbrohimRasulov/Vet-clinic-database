c8c72d8092ecbee8a89c2c0f12725398f57cc57d
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-03-30 20:07:32 +0500

    Create indexes

--- schema.sql
+++ schema.sql
@@ -72,3 +72,7 @@ CREATE TABLE VISITS (
 
 ALTER TABLE owners
 ADD COLUMN email VARCHAR(120);
+
+CREATE INDEX animal_id ON visits (animal_id ASC);
+CREATE INDEX vet_id ON visits (vet_id ASC);
+CREATE INDEX email_id ON owners(email ASC);
